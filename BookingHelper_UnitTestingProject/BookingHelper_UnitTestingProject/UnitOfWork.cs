658eabd03df3f69200d101279ea3de35df77d3db
Hatef Rahimi <hatefraheeme@outlook.com>
2024-05-20 20:33:25 +0200

    Separate the classes

---
+++ BookingHelper_UnitTestingProject/BookingHelper_UnitTestingProject/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BookingHelper_UnitTestingProject
+{
+    public class UnitOfWork
+    {
+        public IQueryable<T> Query<T>()
+        {
+            return new List<T>().AsQueryable();
+        }
+    }
+}
