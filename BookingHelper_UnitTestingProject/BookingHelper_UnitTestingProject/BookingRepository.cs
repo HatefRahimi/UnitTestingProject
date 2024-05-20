7b9a30c93085bbe8bbbe3d8db3da2aee3199209b
Hatef Rahimi <hatefraheeme@outlook.com>
2024-05-20 20:44:57 +0200

    Refactor BookingHelper.cs

---
+++ BookingHelper_UnitTestingProject/BookingHelper_UnitTestingProject/BookingRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace BookingHelper_UnitTestingProject
+{
+    public class BookingRepository : IBookingRepository
+    {
+        public IQueryable<Booking> GetActiveBooking(int? excludedBookingId = null)
+        {
+            var unitOfWork = new UnitOfWork();
+            var bookings =
+                unitOfWork.Query<Booking>()
+                    .Where(
+                        b => b.Status != "Cancelled");
+            if (excludedBookingId.HasValue)
+                bookings.Where(b => b.Id != excludedBookingId.Value);
+
+            return bookings;
+        }
+    }
+}
---
+++ BookingHelper_UnitTestingProject/BookingHelper_UnitTestingProject/IBookingRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace BookingHelper_UnitTestingProject
+{
+    public interface IBookingRepository
+    {
+        IQueryable<Booking> GetActiveBooking(int? excludedBookingId = null);
+    }
+}
